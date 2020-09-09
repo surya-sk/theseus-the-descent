3603949e94b462432e688f1a943cdb9e0f2331a9
Surya <surya.sk05@outlook.com>
2020-09-09 13:42:06 +0530

    Fix audio stopping after reloading scene

--- Assets/Scripts/BackgroundSFX.cs
+++ Assets/Scripts/BackgroundSFX.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,6 +7,10 @@ public class BackgroundSFX : MonoBehaviour
 {
     [SerializeField] AudioSource nightSounds;
 
+    private void Awake()
+    {
+        AudioListener.pause = false;
+    }
     private void Update()
     {
         if (nightSounds.isPlaying == false)
