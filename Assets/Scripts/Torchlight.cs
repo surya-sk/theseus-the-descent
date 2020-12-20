7efd45f16524111452d04f467175e1a7b7bd1569
Surya Kashyap <surya.sk05@outlook.com>
2020-12-20 16:54:11 -0400

    Rewrite logic for showing torch hint

--- Assets/Scripts/Torchlight.cs
+++ Assets/Scripts/Torchlight.cs
@@ -5,18 +5,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Torchlight : MonoBehaviour
+public class Torchlight : MonoBehaviour, ISaveable
 {
     [SerializeField] float intensityDecay = 0.1f;
     [SerializeField] float angleDecay = 1f;
     [SerializeField] float minAngle = 40;
     Light light;
     [SerializeField] AudioClip clip;
+    [SerializeField] Canvas torchCanvas;
+    bool disableTorchCanvas;
 
     private void Start()
     {
         light = GetComponent<Light>();
         light.enabled = false;
+        print(disableTorchCanvas);
     }
 
     private void Update()
@@ -26,6 +29,18 @@ public class Torchlight : MonoBehaviour
         {
             DecreaseLightAngle();
             DecreaseLightIntensity();
+        }
+        if (!disableTorchCanvas)
+        {
+            torchCanvas.enabled = true;
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                disableTorchCanvas = true;
+            }
+        }
+        else
+        {
+            torchCanvas.enabled = false;
         }
     }
 
@@ -67,5 +82,15 @@ public class Torchlight : MonoBehaviour
     public void ResetLightIntensity(float intensityAmount)
     {
         light.intensity = intensityAmount;
+    }
+
+    public object CaptureState()
+    {
+        return disableTorchCanvas;
+    }
+
+    public void RestoreState(object state)
+    {
+        disableTorchCanvas = (bool)state;
     }
 }
