7deb5b192a7f7c2a0d50ae0a263ab6f908ab5ef2
Surya Kashyap <surya.sk05@outlook.com>
2021-01-30 10:45:44 -0400

    Show prompt when player is near note

---
+++ Assets/Scripts/NotePickup.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotePickup : MonoBehaviour
+{
+    [SerializeField]Canvas promptCanvas;
+
+    private void Start()
+    {
+        promptCanvas.enabled = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            promptCanvas.enabled = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            promptCanvas.enabled = false;
+        }
+    }
+}
