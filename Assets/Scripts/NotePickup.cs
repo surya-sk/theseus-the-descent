b70e8ef9bb7e3c2751f5776db2acbf1e42bce5c5
Surya Kashyap <surya.sk05@outlook.com>
2021-01-30 11:53:14 -0400

    Add note interaction loop

--- Assets/Scripts/NotePickup.cs
+++ Assets/Scripts/NotePickup.cs
@@ -1,14 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class NotePickup : MonoBehaviour
 {
-    [SerializeField]Canvas promptCanvas;
+    [SerializeField] Canvas promptCanvas;
+    [SerializeField] TextMeshProUGUI promptText;
+    bool isNearNote;
+    bool isReadingNote;
 
     private void Start()
     {
         promptCanvas.enabled = false;
+        promptText.text = "Press LT to interact";
+        isNearNote = false;
+        isReadingNote = false;
+    }
+
+    private void Update()
+    {
+        if(isNearNote && !isReadingNote)
+        {
+            if (Input.GetMouseButtonDown(1) || Mathf.Round(Input.GetAxisRaw("Fire2")) < 0)
+            {
+                isReadingNote = true;
+            }
+        }
+
+        else if(isReadingNote)
+        {
+            promptText.text = "Press B to exit";
+            if(Input.GetMouseButtonDown(1) || Mathf.Round(Input.GetAxisRaw("Fire2")) < 0)
+            {
+                isReadingNote = false;
+                promptText.text = "Press LT to interact";
+            }
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -16,6 +44,7 @@ public class NotePickup : MonoBehaviour
         if(other.gameObject.tag == "Player")
         {
             promptCanvas.enabled = true;
+            isNearNote = true;
         }
     }
 
