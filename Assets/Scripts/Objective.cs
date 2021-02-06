e3862bb7244ecaf1497c85898d744b4739097a3b
Surya Kashyap <surya.sk05@outlook.com>
2021-02-06 12:24:38 -0400

    Implement basic objective system

---
+++ Assets/Scripts/Objective.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// A class following singleton design pattern to keep track of objectives
+/// </summary>
+public sealed class Objective
+{
+    private static Objective instance = null;
+    private static Queue<string> objectives = new Queue<string>();
+
+    private Objective()
+    {
+        objectives.Enqueue("Objective1");
+        objectives.Enqueue("Objective2");
+        objectives.Enqueue("Objective3");
+    }
+
+    /// <summary>
+    /// Returns the current instance
+    /// </summary>
+    public static Objective Instance
+    {
+        get
+        {
+            if(instance == null)
+            {
+                instance = new Objective();
+            }
+            return instance;
+        }
+    }
+
+    public string GetCurrentObjective()
+    {
+        return objectives.Peek();
+    }
+
+    public void FinishObjective()
+    {
+        objectives.Dequeue();
+    }
+}
---
+++ Assets/Scripts/ObjectiveTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ObjectiveTrigger : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI objectiveText;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        Objective objective = Objective.Instance;
+        objectiveText.text = objective.GetCurrentObjective();
+        print("cplloded");
+        Destroy(gameObject);
+    }
+}
