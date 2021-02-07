3ee7baa55d688c17d8062df5fa6fd58bdc1e177a
Surya Kashyap <surya.sk05@outlook.com>
2021-02-07 10:15:30 -0400

    Implement a more serializable objecitve system

---
+++ Assets/Scripts/ObjectiveTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+/// <summary>
+/// Updates the objective on trigger
+/// </summary>
+public class ObjectiveTrigger : MonoBehaviour
+{
+    bool isFinished = false;
+    [SerializeField] string objective;
+    [SerializeField] TextMeshProUGUI objectiveText;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        objectiveText.text = objective;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        isFinished = true;
+        Destroy(gameObject);
+    }
+}
