53675929c158754abd5edf92ad7bcfba4333974d
surya-sk <surya.sk05@outlook.com>
2021-02-14 16:12:20 -0400

    Add skyboxes and add daynigh scripts

---
+++ Assets/Scripts/DayNight.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNight : MonoBehaviour
+{
+    [SerializeField] Material [] Materials;
+    public Light light;
+    // Start is called before the first frame update
+    void Start()
+    {
+        light = gameObject.GetComponent<Light>();
+        print(gameObject.GetComponent<Light>().intensity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(light.intensity > 0.3f)
+        {
+            light.intensity -= 0.01f;
+        }
+    }
+}
