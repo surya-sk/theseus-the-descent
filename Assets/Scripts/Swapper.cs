f23df52d7bfc568f080e6bac34b9fb42367af323
surya-sk <surya.sk05@outlook.com>
2021-02-17 17:56:17 -0400

    Save swapped object if SaveableObject is attached

--- Assets/Scripts/Swapper.cs
+++ Assets/Scripts/Swapper.cs
@@ -4,16 +4,33 @@ using UnityEngine;
 /// <summary>
 /// A class that swaps the current object with a provided one when an objective is activated
 /// </summary>
-public class Swapper : MonoBehaviour
+public class Swapper : MonoBehaviour, ISaveable
 {
     [SerializeField] GameObject toSwap;
     [SerializeField] GameObject swapWith;
     [SerializeField] GameObject objectiveTrigger;
+    bool hasSwapped = false;
+
+    public object CaptureState()
+    {
+        return hasSwapped;
+    }
+
+    public void RestoreState(object state)
+    {
+        hasSwapped = (bool)state;
+        if(hasSwapped)
+        {
+            Destroy(toSwap);
+            swapWith.SetActive(true);
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if(objectiveTrigger.GetComponent<BoxCollider>().enabled)
         {
+            hasSwapped = true;
             Destroy(toSwap);
             swapWith.SetActive(true);
         }
