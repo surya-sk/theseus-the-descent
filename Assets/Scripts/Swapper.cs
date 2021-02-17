b8aed3289944b07d789084f97b9b0f7b9db5e04c
surya-sk <surya.sk05@outlook.com>
2021-02-16 22:44:11 -0400

    Write swapper class

---
+++ Assets/Scripts/Swapper.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// A class that swaps the current object with a provided one when an objective is activated
+/// </summary>
+public class Swapper : MonoBehaviour
+{
+    [SerializeField] GameObject swapWith;
+    [SerializeField] GameObject objectiveTrigger;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(objectiveTrigger.GetComponent<BoxCollider>().enabled)
+        {
+            Destroy(gameObject);
+            swapWith.SetActive(true);
+        }
+    }
+}
