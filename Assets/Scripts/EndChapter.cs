6242cf2389a2f694592df4493370dea6654e2ced
surya-sk <surya.sk05@outlook.com>
2021-02-21 14:29:45 -0400

    Add end chapter object

---
+++ Assets/Scripts/EndChapter.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndChapter : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        //end chapter
+        print("The end");
+    }
+}
