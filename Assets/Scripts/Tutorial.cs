4e4accaee75e9282043044f69d099be4df6c0127
surya-sk <surya.sk05@outlook.com>
2021-02-21 10:02:03 -0400

    Wind up tutorial

---
+++ Assets/Scripts/Tutorial.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour
+{
+    [SerializeField] Canvas hintCanvas;
+
+    private void Start()
+    {
+        hintCanvas.enabled = false;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        hintCanvas.enabled = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        hintCanvas.enabled = false;
+    }
+}
