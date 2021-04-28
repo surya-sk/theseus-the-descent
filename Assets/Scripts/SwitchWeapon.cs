8f0abbdfc1f888f6a731e25c183a048a05412f61
Surya Kashyap <surya.sk05@outlook.com>
2021-04-28 13:38:54 -0300

    Save equipped weapon

--- Assets/Scripts/SwitchWeapon.cs
+++ Assets/Scripts/SwitchWeapon.cs
@@ -123,5 +123,4 @@ public class SwitchWeapon : MonoBehaviour
         }
 
     }
-
 }
--- Assets/Scripts/WeaponPickup.cs
+++ Assets/Scripts/WeaponPickup.cs
@@ -2,17 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WeaponPickup : MonoBehaviour
+public class WeaponPickup : MonoBehaviour, ISaveable
 {
     SwitchWeapon switchWeapon;
     string[] weaponTags = { "Pistol", "SMG", "Shotgun" };
     [SerializeField] AudioClip pickupClip;
+    int pickedIndex = -1;
 
 
     private void Start()
     {
         switchWeapon = FindObjectOfType<SwitchWeapon>();
     }
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Player")
@@ -23,9 +25,23 @@ public class WeaponPickup : MonoBehaviour
                 {
                     AudioSource.PlayClipAtPoint(pickupClip, gameObject.transform.position);
                     switchWeapon.WeaponAtIndexEnabled(i);
-                    Destroy(gameObject);
+                    pickedIndex = i;
+                    gameObject.transform.position = new Vector3(transform.position.x, -5, transform.position.z);
                 }
             }
         }
+    }
+
+    public object CaptureState()
+    {
+        return pickedIndex;
+    }
+
+    public void RestoreState(object state)
+    {
+        pickedIndex = (int)state;
+        switchWeapon.WeaponAtIndexEnabled(pickedIndex);
+        if(pickedIndex != -1)
+            Destroy(gameObject);
     }
 }
