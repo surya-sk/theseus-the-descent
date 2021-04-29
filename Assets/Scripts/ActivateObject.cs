2deabc494d692088cf817684533b002bfc69ab65
Surya Kashyap <surya.sk05@outlook.com>
2021-04-29 09:17:59 -0300

    Activate second terrain later in the game to improve performance

---
+++ Assets/Scripts/ActivateObject.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class ActivateObject : MonoBehaviour, ISaveable
+{
+    public GameObject targetObject;
+    bool activated = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            activated = true;
+            Activate();
+        }
+    }
+
+    private void Activate()
+    {
+        targetObject.SetActive(true);
+        gameObject.GetComponent<Collider>().enabled = false;
+    }
+
+    public object CaptureState()
+    {
+        return activated;
+    }
+
+    public void RestoreState(object state)
+    {
+        activated = (bool)state;
+        if(activated)
+        {
+            Activate();
+        }
+    }
+}
---
+++ Assets/Scripts/DeactivateObject.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class DeactivateObject : MonoBehaviour, ISaveable
+{
+    public GameObject targetObject;
+    bool deactivated = false;
+
+    public object CaptureState()
+    {
+        return deactivated;
+    }
+
+    public void RestoreState(object state)
+    {
+        deactivated = (bool)state;
+        if(deactivated)
+        {
+            Deactivate();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            deactivated = true;
+            Deactivate();
+        }
+    }
+
+    private void Deactivate()
+    {
+        targetObject.SetActive(false);
+        gameObject.GetComponent<Collider>().enabled = false;
+    }
+}
