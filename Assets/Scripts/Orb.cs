bde235b55a58ee9c8f78924660745b12e339a02c
Surya Kashyap <surya.sk05@outlook.com>
2021-05-13 13:35:26 +0530

    Hook up end game

---
+++ Assets/Scripts/Orb.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Orb : MonoBehaviour
+{
+    public Canvas promptCanvas;
+    public Canvas imageCanvas;
+    public Image fadeOutImage;
+    bool inArea = false;
+    float countdown = 10;
+    bool startTimer = false;
+
+    private void Start()
+    {
+        promptCanvas.enabled = false;
+        imageCanvas.enabled = false;
+        fadeOutImage.canvasRenderer.SetAlpha(0.0f);
+    }
+    private void Update()
+    {
+        if(inArea)
+        {
+            if (Input.GetMouseButtonDown(1) || Mathf.Round(Input.GetAxisRaw("Fire2")) < 0)
+            {
+                imageCanvas.enabled = true;
+                fadeOutImage.CrossFadeAlpha(1, 2, false);
+                startTimer = true;
+            }
+        }
+
+        if(startTimer)
+        {
+            if(countdown > 0)
+            {
+                countdown -= Time.deltaTime;
+            }
+            else
+            {
+                SceneManager.LoadScene(9);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            promptCanvas.enabled = true;
+            inArea = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            promptCanvas.enabled = false;
+            inArea = false;
+        }
+    }
+}
